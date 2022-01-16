3b8b86769b404633c7ed447f19d3277e91b6f4e1
Edwike12 <nyaunchoedwike@gmail.com>
2022-01-16 19:11:38 +0300

    set up database using sqlalchemy

--- app/auth.py
+++ app/auth.py
@@ -1,15 +1,16 @@
-from flask import Blueprint,  render_template,  redirect, url_for
+from flask import Blueprint,  render_template,  redirect, url_for, request
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods= ['GET', 'POST'])
 def login():
     return  render_template("login.html")
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods= ['GET', 'POST'])
 def sign_up():
+    username = request.form.get("username")
     return  render_template("signup.html")
 
-@auth.route("/logout")
+@auth.route("/logout", methods= ['GET', 'POST'])
 def logout():
     return  redirect(url_for("views.home"))
