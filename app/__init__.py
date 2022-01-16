adc74f5aca2b165ef6eb963c2777fc2c81371428
Edwike12 <nyaunchoedwike@gmail.com>
2022-01-16 14:55:32 +0300

    created a flask route

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ def create_app():
     app= Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
 
-
+    @app.route("/")
+    def home():
+        return "<h1>HELLO WORLD</h1>"
 
     return app
