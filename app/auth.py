0a285d87b9b47d453d1768340cc583920b6e7595
Edwike12 <nyaunchoedwike@gmail.com>
2022-01-16 15:37:39 +0300

    imported render template from flask

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint,  render_template
 
 
 auth = Blueprint("auth", __name__)
@@ -6,13 +6,13 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    return render_template()
+    return "login"
 
 @auth.route("/sign-up")
 def sign_up():
-    return render_template()
+    return "signup"
 
 
 @auth.route("/logout")
 def logout():
-    return redirect(url_for())
+    return "logout"
