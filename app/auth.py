0c635de9bb53094b9d69c294de028b07d263e829
Edwike12 <nyaunchoedwike@gmail.com>
2022-01-16 20:18:35 +0300

    created a login logic

--- app/auth.py
+++ app/auth.py
@@ -8,7 +8,22 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods= ['GET', 'POST'])
 def login():
-    return  render_template("login.html")
+     if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in!", category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Password is incorrect.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+            
+            return  render_template("login.html")
 
 @auth.route("/sign-up", methods= ['GET', 'POST'])
 def sign_up():
